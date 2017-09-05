2767e3615068ce72aa1b4c934412710762614a91
Todd Merritt <tmerritt@email.arizona.edu>
2017-09-05 16:49:39 -0700

    Merge current ratings with player data to get previous ELO

--- calculate_elo.R
+++ calculate_elo.R
@@ -1,10 +1,23 @@
-for (pos in c("QUARTERBACK" ,"RUNNING_BACK" ,"WIDE_RECEIVER" ,"TIGHT_END" ,"DEFENSIVE_LINEMAN" ,"LINEBACKER" ,"DEFENSIVE_BACK" ,"KICKOFF_KICKER" ,"KICK_RETURNER" ,"PUNTER" ,"PUNT_RETURNER" ,"FIELD_GOAL_KICKER")) {
+
+# read in original elo values if they exist
+prv = read.csv("current_ratings.csv")
+
+#for (pos in c("QUARTERBACK" ,"RUNNING_BACK" ,"WIDE_RECEIVER" ,"TIGHT_END" ,"DEFENSIVE_LINEMAN" ,"LINEBACKER" ,"DEFENSIVE_BACK" ,"KICK_RETURNER" ,"PUNT_RETURNER" ,"FIELD_GOAL_KICKER")) {
+for (pos in c("QUARTERBACK")) {
   today = format(Sys.time(), "%Y-%m-%d")
   file = paste(pos, today, sep="_")
   file = paste(file, "csv", sep=".")
   mydata = read.csv(file)
 
-  #print(mydata)
+  # merge in current ELO scores
+  scored_pos_data <- merge(x = mydata, y = prv, by = "Player", all.x = TRUE)
+
+  # calculate new ELO scores by position
+
+  print(scored_pos_data)
 
   rm(mydata)
+  rm(scored_pos_data)
 }
+
+write.csv(updated_ratings, file = "new_ratings.csv",row.names=FALSE)
