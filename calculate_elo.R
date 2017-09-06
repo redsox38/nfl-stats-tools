21a75a0038efcbdea895d319dd2bde7758759829
Todd Merritt <tmerritt@email.arizona.edu>
2017-09-05 18:31:41 -0700

    Set default elo scores for NA values when writing output

--- calculate_elo.R
+++ calculate_elo.R
@@ -1,26 +1,28 @@
 
+default_elo = "1500"
+
 # read in original elo values if they exist
 prv = read.csv("current_ratings.csv")
-
-# create empty data frame for updates
-updated_ratings = prv[FALSE,]
 
 #for (pos in c("QUARTERBACK" ,"RUNNING_BACK" ,"WIDE_RECEIVER" ,"TIGHT_END" ,"DEFENSIVE_LINEMAN" ,"LINEBACKER" ,"DEFENSIVE_BACK" ,"KICK_RETURNER" ,"PUNT_RETURNER" ,"FIELD_GOAL_KICKER")) {
 for (pos in c("QUARTERBACK")) {
   today = format(Sys.time(), "%Y-%m-%d")
   file = paste(pos, today, sep="_")
   file = paste(file, "csv", sep=".")
+  outfile = paste("new", pos, "ratings", sep="_")
+  outfile = paste(outfile, "csv", sep=".")
+
   mydata = read.csv(file)
 
   # merge in current ELO scores
   scored_pos_data <- merge(x = mydata, y = prv, by = "Player", all.x = TRUE)
+  rm(mydata)
 
   # calculate new ELO scores by position
 
-  print(scored_pos_data)
+  
+  write.csv(scored_pos_data[,c("Player","ELO")], file = outfile, na=default_elo, row.names=FALSE)
 
-  rm(mydata)
   rm(scored_pos_data)
 }
 
-write.csv(updated_ratings, file = "new_ratings.csv",row.names=FALSE)
